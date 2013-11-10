66bc606a112162332b2bdf13cc1f49b670957543
kiku1monzi3622 <team.ganeme@gmail.com>
2013-11-11 01:02:01 +0900

    sp settings add

---
+++ sp/js/main.js
@@ -0,0 +1,54 @@
+document.addEventListener("touchstart",function(){},true);
+
+//https://developers.google.com/mobile/articles/fast_buttons
+google.ui.FastButton = function(element, handler) {
+  this.element = element;
+  this.handler = handler;
+
+  element.addEventListener('touchstart', this, false);
+  element.addEventListener('click', this, false);
+};
+
+google.ui.FastButton.prototype.handleEvent = function(event) {
+  switch (event.type) {
+    case 'touchstart': this.onTouchStart(event); break;
+    case 'touchmove': this.onTouchMove(event); break;
+    case 'touchend': this.onClick(event); break;
+    case 'click': this.onClick(event); break;
+  }
+};
+
+//touchstart
+google.ui.FastButton.prototype.onTouchStart = function(event) {
+  event.stopPropagation();
+
+  this.element.addEventListener('touchend', this, false);
+  document.body.addEventListener('touchmove', this, false);
+
+  this.startX = event.touches[0].clientX;
+  this.startY = event.touches[0].clientY;
+};
+
+//touchmove
+google.ui.FastButton.prototype.onTouchMove = function(event) {
+  if (Math.abs(event.touches[0].clientX - this.startX) > 10 ||
+      Math.abs(event.touches[0].clientY - this.startY) > 10) {
+    this.reset();
+  }
+};
+
+//touchend
+google.ui.FastButton.prototype.onClick = function(event) {
+  event.stopPropagation();
+  this.reset();
+  this.handler(event);
+
+  if (event.type == 'touchend') {
+    google.clickbuster.preventGhostClick(this.startX, this.startY);
+  }
+};
+
+google.ui.FastButton.prototype.reset = function() {
+  this.element.removeEventListener('touchend', this, false);
+  document.body.removeEventListener('touchmove', this, false);
+};
